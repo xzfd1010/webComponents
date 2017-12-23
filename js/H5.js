16e270530fff433ab78be93d653547b2e6e27cec
Nick <zhongcheng509@gmail.com>
2017-12-23 18:53:27 +0800

    回弹参数修改

--- js/H5.js
+++ js/H5.js
@@ -106,8 +106,9 @@ var H5 = function () {
     // firstPage:初始第几页
     this.loader = function (firstPage) {
         this.el.fullpage({
+            scrollingSpeed:900,
             css3:true,
-            easingcss3: 'cubic-bezier(.17,.67,.53,1.33)',
+            easingcss3: 'cubic-bezier(.17,.67,.4,1.15)',
             onLeave: function (index, nextIndex, direction) {
                 //建立了一种耦合的方式，这样就可以给每个页面单独写事件
                 $(this).find('.h5_component').trigger('onLeave');
